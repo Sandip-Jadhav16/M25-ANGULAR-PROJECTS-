1dc935751342188295a5de78a823ba85617f1067
Sandip Jadhav <96832996+Sandip-Jadhav16@users.noreply.github.com>
2022-12-16 20:41:55 +0530

    Add files via upload

---
+++ Pipes/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Pipes';
+    students: any[]= [
+    {
+      ID:101,Name:"Sandip Jadhav",Gender:"male",City:"pune",Fee:35000
+    },
+    {
+      ID:102,Name:"Mohan Gabhale",Gender:"male",City:"mumbai",Fee:25000
+    },
+    {
+      ID:103,Name:"Bhagyashree Marathe",Gender:"female",City:"buldhana",Fee:45000
+    }];
+
+    today:number=Date.now();
+}
